4422b2d91fc15d7e18382dcc6fedddd3911aae2c
Tiago Almeida <jumpifzero@gmail.com>
2018-02-10 15:37:42 +0000

    Changed from .app to .test as this has less issues on latest Chrome

--- ui5-create.sh
+++ ui5-create.sh
@@ -2,13 +2,13 @@
 #
 # This script will create a folder with that name under your projects folder.
 # It will then clone a git repo into it and enable an apache website serving those files.
-# The website will be available under a url something.app.com (which points to localhost)
+# The website will be available under a url something.test (which points to localhost)
 #
 
 #SCRIPT_FOLDER=`dirname "$BASH_SOURCE"`
 SCRIPT_FOLDER=$(dirname $(readlink -f "$BASH_SOURCE"))
 PROJECTS_FOLDER=~/projectos/
-UI5_RESOURCES_FOLDER=/home/tiago/lib/ui5/sapui5-1.40.12/resources
+UI5_RESOURCES_FOLDER=/home/tiago/lib/ui5/1.52.5/resources
 
 echo Running from ${SCRIPT_FOLDER}
 
@@ -21,8 +21,8 @@ read -p 'App name: ' app_name
 
 # Copy the apache .conf template file into the apache sites available folder.
 sed -e "s/%USER%/${USER}/g;s/%APP_NAME%/${app_name}/g" \
-    ${SCRIPT_FOLDER}/ui5-create-files/site-available-template.conf > ${app_name}.app.com.conf
-sudo mv ${app_name}.app.com.conf /etc/apache2/sites-available/
+    ${SCRIPT_FOLDER}/ui5-create-files/site-available-template.conf > ${app_name}.test.conf
+sudo mv ${app_name}.test.conf /etc/apache2/sites-available/
 
 echo Please enter the url for the git repository holding the application.
 read -p 'git repository: ' git_repo_url
@@ -40,9 +40,9 @@ sudo ln -s -T ${UI5_RESOURCES_FOLDER} resources
 
 # Enable the website
 echo Enabling the site in apache
-sudo a2ensite ${app_name}.app.com.conf
+sudo a2ensite ${app_name}.test.conf
 sudo service apache2 reload
 
 # Create an entry on /etc/hosts
-echo Create an entry on /etc/hosts pointing ${app_name}.app to 127.0.0.1
-echo -e "\n127.0.0.1\t${app_name}.app"
+echo Create an entry on /etc/hosts pointing ${app_name}.test to 127.0.0.1
+echo -e "\n127.0.0.1\t${app_name}.test"
